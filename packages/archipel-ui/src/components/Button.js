f07ba5be59ced56691cb4595d71242258ec650b5
Franz Heinzmann <frando@unbiskant.org>
2018-09-05 18:12:51 +0200

    Remove debug code

--- packages/archipel-ui/src/components/Button.js
+++ packages/archipel-ui/src/components/Button.js
@@ -4,7 +4,6 @@ import { cls, proplist } from '../util.js'
 import './Button.pcss'
 
 const Button = (props) => {
-  console.log('button props!', props)
   return (
     <button {...props} className={cls(props, 'btn')}>
       {props.children}
