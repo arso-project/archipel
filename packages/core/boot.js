9aaa442828f1561ec4ea76a37d5bb68441d1d129
Franz Heinzmann (Frando) <frando@unbiskant.org>
2018-10-30 21:51:55 +0100

    Restructure boot: Move config handling into function.

--- packages/core/boot.js
+++ packages/core/boot.js
@@ -12,50 +12,84 @@ const Rootspace = require('./lib/rootspace')
 module.exports = boot
 
 async function boot (opts) {
-  // read out process.argv to get start parameters:
+  opts = opts || {}
+
+  const core = ucore()
+
+  core.use(config, opts)
+  core.after(setupServer)
+  await core.ready()
+
+  let httpPort = core.config('httpPort')
+  if (httpPort) {
+    core.httpServer.listen(httpPort, console.log(`Server listening on port ${httpPort}`))
+  }
+
+  return core
+
+  function setupServer () {
+    if (core.config('httpPort')) {
+      core.register(server, { staticPath: core.config('staticPath') })
+    }
+    core.register(rpc, core.config('rpc'))
+    core.use(archipel, { dbPath: core.config('dbPath') })
+    core.use(workspace)
+    core.register(drive)
+    core.register(graph)
+  }
+}
+
+async function config (core, opts) {
+  const defaultConfig = {
+    noHttp: false,
+    httpPort: 8080,
+    dbPath: p.join(__dirname, '../..', '.db'),
+    cliConfig: true,
+    staticPath: process.env.ARCHIPEL_STATIC_PATH || p.join(__dirname, '../app/dist/web'),
+    rpc: {
+      prefix: '/ucore'
+    }
+  }
+
+  let config = Object.assign({}, defaultConfig, opts)
+
+  if (config.cliConfig) {
+    config = Object.assign({}, config, getCliConfig())
+  }
+
+  core.decorate('config', (key) => {
+    return key ? config[key] : config
+  })
+}
+
+async function archipel (core, opts) {
+  let archiveTypes
+  core.decorate('registerArchiveType', (newTypes) => {
+    archiveTypes = { ...archiveTypes, ...newTypes }
+  })
+  core.decorate('getArchiveTypes', () => archiveTypes)
+
+  core.decorate('root', Rootspace(opts.dbPath))
+  core.ready(() => {
+    core.root.registerArchiveTypes(archiveTypes)
+  })
+}
+
+function getCliConfig () {
+  console.log(process.cwd())
   let args = {}
   process.argv.slice(2).forEach(function (val, index, array) {
-    // console.log(index + ': ' + val)
     let arg = val.split(/[=:]/)
     Object.assign(args, JSON.parse(`{ "${arg[0]}":"${arg[1]}"}`))
   })
   let { port, dbpath, ...rest } = args
-  port = Number(port) || 8080
-  dbpath = dbpath || '.db'
-  process.env.ARCHIPEL_DB_PATH = p.join(__dirname, '../..', dbpath)
 
-  // actual programm code:
-  opts = opts || {}
-  const core = ucore()
-  if (!opts.noHttp) core.register(server, { staticPath: process.env.ARCHIPEL_STATIC_PATH })
+  dbpath = dbpath || process.env.ARCHIPEL_DB_PATH
+  port = port || process.env.ARCHIPEL_HTTP_PORT
 
-  let rpcOpts = { prefix: '/ucore' }
-  if (opts.rpc) rpcOpts = { ...rpcOpts, ...opts.rpc }
-  core.register(rpc, rpcOpts)
-
-  core.use(archipel)
-  core.use(workspace)
-  core.register(drive)
-  core.register(graph)
-
-  await core.ready()
-  if (!opts.noHttp) core.httpServer.listen(port, console.log(`Server listening on port ${port}`))
-
-  return core
-}
-
-async function archipel (core, opts) {
-  let dbPath = process.env.ARCHIPEL_DB_PATH || p.join(__dirname, '../..', '.db')
-  dbPath = p.resolve(dbPath)
-
-  let archiveTypes
-  core.decorate('registerArchiveType', (newTypes) => {
-    archiveTypes = {...archiveTypes, ...newTypes}
-  })
-  core.decorate('getArchiveTypes', () => archiveTypes)
-
-  core.decorate('root', Rootspace(dbPath))
-  core.ready(() => {
-    core.root.registerArchiveTypes(archiveTypes)
-  })
+  let config = {}
+  if (port) config.httpPort = Number(port)
+  // Todo: Too much magic with going two dirs up.
+  if (dbpath) config.dbPath = p.join(process.cwd(), '../..', dbpath)
+  return config
 }
