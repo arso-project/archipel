6546621e6baa1c759d0d139c4d6cb85450411756
mororor <matthias@syndikat.org>
2018-11-30 14:03:44 +0100

    restored Checkbox components functionality

--- packages/ui/src/components/Checkbox.js
+++ packages/ui/src/components/Checkbox.js
@@ -8,7 +8,7 @@ import './Checkbox.pcss'
 const Checkbox = (props) => {
   let { id, label, info, ...rest } = props
   return (
-    <label htmlFor={id} className={cls(rest, 'chb')}>
+    <label htmlFor={id} {...rest} className={cls(rest, 'chb')}>
       <input type='checkbox' value='1' id={id} />
       <MdCheck size={24} />
       <span>{label}</span>
