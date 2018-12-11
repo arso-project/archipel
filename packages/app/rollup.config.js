7dcfdfff1e3db8fa7fd642ff0caea561f2ffff23
Franz Heinzmann (Frando) <frando@unbiskant.org>
2018-12-11 02:08:25 +0100

    Update react, add rollup livereload.

--- packages/app/rollup.config.js
+++ packages/app/rollup.config.js
@@ -7,9 +7,60 @@ import globals from 'rollup-plugin-node-globals'
 import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
 import copy from 'rollup-plugin-copy-glob'
+import livereload from 'rollup-plugin-livereload'
 // import visualizer from 'rollup-plugin-visualizer'
 
 let isDev = process.env.NODE_ENV === 'development'
+
+const plugins = [
+  // peerDepsExternal(),
+  replace({
+    delimiters: ['', ''],
+    exclude: /rollup-plugin-node-builtins/,
+    values: {
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'require(\'readable-stream/transform\')': 'require(\'stream\').Transform',
+      'require("readable-stream/transform")': 'require("stream").Transform',
+      'require("readable-stream")': 'require("stream")',
+      'require(\'readable-stream\')': 'require(\'stream\')',
+      'readable-stream': 'stream',
+
+      // Fix bundling bug in inherits.
+      'ctor.prototype = Object.create(superCtor.prototype, {': 'ctor.prototype = Object.create(superCtor ? superCtor.prototype : {}, {',
+      // Fix bundling bug in avvio.
+      'const cache = require.cache': 'const cache = require.cache || {}'
+    }
+  }),
+  json(),
+  postcss({
+    extract: true
+  }),
+  sucrase({
+    exclude: ['**/node_modules/**', 'node_modules/**', /node_modules/],
+    transforms: ['jsx']
+  }),
+  resolve({
+    browser: true,
+    preferBuiltins: true
+  }),
+  commonjs({
+    namedExports: {
+      'react': ['Children', 'Component', 'PropTypes', 'createElement', 'Fragment'],
+      'react-dom': ['render']
+    },
+    include: /.*/
+  }),
+  globals(),
+  builtins(),
+  // visualizer(),
+  copy([
+    { files: 'assets/**', dest: 'dist' }
+  ], { watch: true })
+]
+
+if (isDev) {
+  plugins.push(livereload())
+}
 
 export default {
   input: 'render.js',
@@ -22,49 +73,5 @@ export default {
     'readable-stream',
     'readable-stream/transform'
   ],
-  plugins: [
-    // peerDepsExternal(),
-    replace({
-      delimiters: ['', ''],
-      exclude: /rollup-plugin-node-builtins/,
-      values: {
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        'require(\'readable-stream/transform\')': 'require(\'stream\').Transform',
-        'require("readable-stream/transform")': 'require("stream").Transform',
-        'require("readable-stream")': 'require("stream")',
-        'require(\'readable-stream\')': 'require(\'stream\')',
-        'readable-stream': 'stream',
-
-        // Fix bundling bug in inherits.
-        'ctor.prototype = Object.create(superCtor.prototype, {': 'ctor.prototype = Object.create(superCtor ? superCtor.prototype : {}, {',
-        // Fix bundling bug in avvio.
-        'const cache = require.cache': 'const cache = require.cache || {}'
-      }
-    }),
-    json(),
-    postcss({
-      extract: true
-    }),
-    sucrase({
-      exclude: /node_modules/,
-      transforms: ['jsx']
-    }),
-    resolve({
-      browser: true,
-      preferBuiltins: true
-    }),
-    commonjs({
-      namedExports: {
-        'react': ['Children', 'Component', 'PropTypes', 'createElement'],
-        'react-dom': ['render']
-      },
-      include: /.*/
-    }),
-    globals(),
-    builtins(),
-    // visualizer(),
-    copy([
-      { files: 'assets/**', dest: 'dist' }
-    ], { watch: true })
-  ]
+  plugins
 }
