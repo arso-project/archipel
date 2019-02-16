7dc72dbcc160c24f4d2ca9a34e13851ca9082533
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-02-16 18:09:30 +0100

    Fix default static path.

--- packages/backend/config.js
+++ packages/backend/config.js
@@ -1,7 +1,7 @@
 const config = {
   server: {
     port: process.env.NODE_PORT || 8080,
-    static: process.env.ARCHIPEL_STATIC_PATH || '../frontend/dist'
+    static: process.env.ARCHIPEL_STATIC_PATH || '../app/dist'
   },
   library: {
     path: process.env.ARCHIPEL_DB_PATH || '.db'
