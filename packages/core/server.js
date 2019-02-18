9343888636694b43deed8b37dc1d2bacc638fd38
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-02-18 12:37:42 +0100

    Remove old core package.

--- packages/core/server.js
+++
@@ -1,8 +0,0 @@
-const boot = require('./boot')
-
-boot()
-
-process.on('unhandledRejection', (error, p) => {
-  throw error
-  // console.log('Unhandled Rejection at: Promise', p, 'reason:', error)
-})
