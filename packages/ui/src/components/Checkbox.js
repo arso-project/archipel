619b024b29b7361507b78bec721f9749e472de01
mororor <matthias@syndikat.org>
2019-02-19 19:27:03 +0100

    Checkbox improvement: set checked state

--- packages/ui/src/components/Checkbox.js
+++ packages/ui/src/components/Checkbox.js
@@ -6,10 +6,10 @@ import { InfoPopup } from '../'
 import './Checkbox.pcss'
 
 const Checkbox = (props) => {
-  let { id, label, info, ...rest } = props
+  let { id, label, info, onChange, checked, ...rest } = props
   return (
     <label htmlFor={id} {...rest} className={cls(rest, 'chb')}>
-      <input type='checkbox' value='1' id={id} />
+      <input type='checkbox' value='1' id={id} onChange={onChange} checked={checked} />
       <MdCheck size={24} />
       <span>{label}</span>
       {info ? <InfoPopup info={info} /> : ''}
