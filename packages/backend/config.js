634d8288c55ceb9862c1f6718f847a903d253b8e
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-02-22 13:36:21 +0100

    Fixes for dokku.

--- packages/backend/config.js
+++ packages/backend/config.js
@@ -1,6 +1,6 @@
 const config = {
   server: {
-    port: process.env.NODE_PORT || 8080,
+    port: process.env.NODE_PORT || process.env.PORT || 8080,
     host: process.env.NODE_HOST || '127.0.0.1',
     static: process.env.ARCHIPEL_STATIC_PATH || '../app/dist'
   },
