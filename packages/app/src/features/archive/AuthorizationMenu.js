7724093af2679834a236811e79f4ea872fd8aaf5
mororor <matthias@syndikat.org>
2019-02-28 21:18:51 +0100

    AuthorizationMenu some changes

--- packages/app/src/features/archive/AuthorizationMenu.js
+++ packages/app/src/features/archive/AuthorizationMenu.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Foldable, Button, InfoPopup } from '@archipel/ui'
+import { Button, Checkbox, InfoPopup, ExpandButton, StructuresCheckList } from '@archipel/ui'
 
 import { withApi } from '../../lib/api.js'
-import { Checkbox, StructuresCheckList } from '../../../../ui/src/index.js';
 
 class AuthorizationMenuInner extends React.Component {
   constructor (props) {
@@ -12,7 +11,8 @@ class AuthorizationMenuInner extends React.Component {
       authObj: null,
       archive: null,
       selected: {},
-      notRequested: {}
+      notRequested: {},
+      expanded: false
     }
     this.api = this.props.api.hyperlib
     // this.onSelect = this.onSelect.bind(this)
@@ -29,7 +29,7 @@ class AuthorizationMenuInner extends React.Component {
       }
       let archive = await this.api.openArchive({ key: authObj.primaryKey }) || null
       this.setState({ authObj, archive, authCipher: null, selected: {} })
-      this.checklistElements(archive, authObj)
+      this.checklistElementsAndDefaultSelect(archive, authObj)
     }
   }
 
@@ -48,38 +48,72 @@ class AuthorizationMenuInner extends React.Component {
     this.setState({ authCipher, authObj: null, archive: null, selected: {}, notRequested: {} })
   }
 
-  async checklistElements (archive, authObj) {
-    let notRequested = {}
-    let selected = {}
-    notRequested[archive.key] = !authObj.structures.find((s) => (s === archive.key))
+  async checklistElementsAndDefaultSelect (archive, authObj) {
+    let { selected, notRequested, expanded } = this.state
+
+    // primary
+    let tmpBool = !!authObj.structures.find((s) => (s === archive.key))
+    console.log(tmpBool)
+    notRequested[archive.key] = !tmpBool
+    selected[archive.key] = tmpBool
+
+    // structures
     for (let i of archive.structures) {
-      notRequested[i.key] = !authObj.structures.find((s) => (s === i.key))
-      selected[i.key] = false
+      tmpBool = !!authObj.structures.find((s) => (s === i.key))
+      notRequested[i.key] = !tmpBool
+      selected[i.key] = tmpBool
     }
-    this.setState({ notRequested, selected })
+
+    expanded = Object.values(notRequested).reduce((acc, val) => acc || val)
+
+    this.setState({ notRequested, selected, expanded })
   }
 
   async onSelect (bool, key) {
-    let { selected } = this.state
+    let { selected, expanded } = this.state
+    if (!expanded) return this.switchAll()
     selected[key] = bool
     this.setState({ selected })
   }
 
+  async switchAll (toBeState) {
+    let { selected } = this.state
+    let { archive } = this.props
+    if (!archive) return
+
+    if (toBeState === undefined || toBeState === null) {
+      toBeState = !selected[archive.key]
+    }
+
+    selected[archive.key] = toBeState
+    for (let i of archive.structures) {
+      selected[i.key] = toBeState
+    }
+    await this.setState({ selected })
+  }
+
+  onExpand () {
+    let { expanded, selected } = this.state
+    let { archive } = this.props
+    this.setState({ expanded: !expanded })
+    this.switchAll(selected[archive.key])
+  }
+
   render () {
-    let { authCipher, authObj, archive, notRequested, selected } = this.state
+    let { authCipher, authObj, archive, notRequested, selected, expanded } = this.state
     let heading = 'Authorize write access'
     let content
     if (!authObj) {
       // Enter Authorization Cypher
       content = (
         <div>
-          <div className='flex flex-col w-40'>
+          <div className='flex flex-col w-auto'>
             <strong htmlFor='inputAuthorizationCipher'
               className='m-1 mb-2'>
               Enter cipher:
               <InfoPopup wInfo='w-48' info='To authorize others to write to your archives, they need to generate a request cipher. If you received one, you may enter it below.' />
             </strong>
-            <textarea id='inputAuthorizationCipher' name='inputAuthorizationCipher'
+            <textarea className='border' id='inputAuthorizationCipher' name='inputAuthorizationCipher'
               placeholder='Authorization Cipher'
               value={authCipher || ''} cols='10' rows='8'
               onChange={(e) => this.setState({ authCipher: e.target.value })}
@@ -109,34 +143,37 @@ class AuthorizationMenuInner extends React.Component {
       content = (
         <>
           <div className='flex flex-col'>
-            { authObj.userMessage
-              ? <div className='flex flex-col'>
-                <strong className='pl-1 pt-1 pb-1'>User Message:</strong>
-                <textarea readOnly className='w-40' value={authObj.userMessage} />
-              </div>
-              : <span>no user message</span>}
-            <strong className='pl-1 pt-1 pb-1'>
+            <div className='flex flex-col'>
+              <strong className='pl-1 pt-1 pb-1'>User Message:</strong>
+              <textarea readOnly placeholder='no user message' className='border resize-none overflow-y-scroll' value={authObj.userMessage || ''} />
+            </div>
+            <strong className='mt-2 pl-1 pt-1 pb-1'>
             Please select:
               <InfoPopup wInfo='w-48' info='Select those structures you would like to give authorization for and klick "Authorize". If some are greyed out, they were not requested.' />
             </strong>
             <div className='flex flex-col p-1 m-1 bg-white border-none rounded'>
-              <Checkbox id='authMenuPrimCheck' label={archive.info.title + '/' + archive.type}
-                checked={selected[archive.key] || false}
-                onChange={(e) => this.onSelect(e.target.checked, archive.key)} disabled={notRequested[archive.key]} />
-              <div className='pl-4'>
-                <StructuresCheckList structures={archive.structures}
-                  idSub='authMenuItems'
-                  onSelect={this.onSelect.bind(this)}
-                  disabled={notRequested} selected={selected} />
+              <div className='flex'>
+                <Checkbox className='flex-1 text-lg'
+                  id='authMenuPrimCheck' label={archive.info.title + '/' + archive.type}
+                  checked={selected[archive.key] || false}
+                  onChange={(e) => this.onSelect(e.target.checked, archive.key)} disabled={notRequested[archive.key]} />
+                <ExpandButton expanded={expanded} size={24} onClick={() => this.onExpand()} />
               </div>
+              <StructuresCheckList
+                indent='4'
+                expanded={expanded}
+                structures={archive.structures}
+                idSub='authMenuItems'
+                onSelect={this.onSelect.bind(this)}
+                disabled={notRequested} selected={selected} />
             </div>
           </div>
           <div className='flex'>
-            <Button className='m-1 p-1'
+            <Button className='m-1'
               onClick={() => this.setState({ authObj: null })} >
               Back
             </Button>
-            <Button className='m-1 p-1'
+            <Button className='m-1'
               onClick={() => this.submitAuthorization()} >
               Authorize
             </Button>
