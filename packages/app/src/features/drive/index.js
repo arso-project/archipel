5e3d8d5e9a7c98cf906b4b91223dd50ca3185b72
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-02-28 12:41:07 +0100

    Add missing file.

---
+++ packages/app/src/features/drive/index.js
@@ -0,0 +1,22 @@
+import { registerRoute, registerElement } from '../../lib/router'
+import { FilePage, FilePageWrapper } from './FsScreen.js'
+import UploadFile from './UploadFile'
+import CreateDir from './CreateDir'
+
+import { MdCreateNewFolder, MdInsertDriveFile } from 'react-icons/md'
+
+registerRoute('archive/:archive/file', FilePage, { Wrapper: FilePageWrapper })
+registerRoute('archive/:archive/file/*', FilePage)
+
+registerElement('archive/:archive', {
+  link: [
+    { name: 'Files', href: 'archive/:archive/file', weight: -8 }
+  ]
+})
+
+registerElement('archive/:archive/file', {
+  actions: [
+    { name: 'Upload files', component: UploadFile, icon: MdInsertDriveFile, match: file => file.isDirectory },
+    { name: 'Create directory', component: CreateDir, icon: MdCreateNewFolder, match: file => file.isDirectory }
+  ]
+})
