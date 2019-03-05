398bdf470227b822708760a9f4e852cbbb7b2c6e
Franz Heinzmann (Frando) <frando@unbiskant.org>
2019-03-05 15:31:56 +0100

    Fix markdown plugin.

--- packages/plugin-markdown/frontend.js
+++ packages/plugin-markdown/frontend.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import remark from 'remark'
 import reactRenderer from 'remark-react'
+import registry from '@archipel/app/src/lib/component-registry'
 
 import './github-markdown.css'
 
-export default {
-  name: 'markdown',
-  plugin
-}
-
-async function plugin (core) {
-  core.components.add('fileViewer', MarkdownViewer, {
+export default function init () {
+  registry.add('fileViewer', MarkdownViewer, {
     stream: false,
     format: 'utf-8',
     match: (file) => {
